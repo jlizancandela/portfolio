2b402d05354f56507187d3bb10e74799211a954e
jorge lizan candela <jlizancandela@gmail.com>
2024-05-06 13:04:46 +0200

    primer commit

---
+++ README.md
@@ -0,0 +1,23 @@
+---
+
+# Portfolio Personal con Astro, HTML, CSS y JavaScript
+
+Este repositorio contiene mi portfolio personal, diseñado y desarrollado con Astro, HTML, CSS y JavaScript. Está basado en el esquema JSON del CV de [jsonresume.org](https://jsonresume.org), adaptado al diseño de [Bartosz Jarocki](https://github.com/BartoszJarocki/cv) y al tutorial de [midulive](https://www.youtube.com/watch?v=Zwh92LTB-Bk&t=5697s) con su [repositorio asociado](https://github.com/midudev/minimalist-portfolio-json).
+
+## Capturas de Pantalla
+
+Inserta aquí capturas de pantalla de tu portfolio para mostrar cómo se ve.
+
+## Características
+
+- **Astro**: Utiliza Astro para la generación de sitios web estáticos, lo que proporciona un rendimiento óptimo y una mejor experiencia de desarrollo.
+- **HTML, CSS y JavaScript**: La estructura y el estilo del sitio están creados con HTML, CSS y JavaScript para una navegación suave y una presentación elegante.
+- **Basado en JSON Resume**: Utiliza el esquema JSON de [jsonresume.org](https://jsonresume.org) para estructurar la información de mi CV de manera estandarizada y fácilmente actualizable.
+
+## Instalación y Uso
+
+1. Clona este repositorio en tu máquina local.
+   ```bash
+   git clone https://github.com/tu_usuario/tu_portfolio.git
+   ```
+2. Abre el archivo `index.html` en tu navegador web para ver el portfolio.
---
+++ src/helper/changemode.ts
@@ -0,0 +1,14 @@
+
+export const darkIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(255, 255, 255, 1);transform: ;msFilter:;"><path d="M20.742 13.045a8.088 8.088 0 0 1-2.077.271c-2.135 0-4.14-.83-5.646-2.336a8.025 8.025 0 0 1-2.064-7.723A1 1 0 0 0 9.73 2.034a10.014 10.014 0 0 0-4.489 2.582c-3.898 3.898-3.898 10.243 0 14.143a9.937 9.937 0 0 0 7.072 2.93 9.93 9.93 0 0 0 7.07-2.929 10.007 10.007 0 0 0 2.583-4.491 1.001 1.001 0 0 0-1.224-1.224zm-2.772 4.301a7.947 7.947 0 0 1-5.656 2.343 7.953 7.953 0 0 1-5.658-2.344c-3.118-3.119-3.118-8.195 0-11.314a7.923 7.923 0 0 1 2.06-1.483 10.027 10.027 0 0 0 2.89 7.848 9.972 9.972 0 0 0 7.848 2.891 8.036 8.036 0 0 1-1.484 2.059z"></path></svg>`;
+export const lightIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;"><path d="M6.995 12c0 2.761 2.246 5.007 5.007 5.007s5.007-2.246 5.007-5.007-2.246-5.007-5.007-5.007S6.995 9.239 6.995 12zM11 19h2v3h-2zm0-17h2v3h-2zm-9 9h3v2H2zm17 0h3v2h-3zM5.637 19.778l-1.414-1.414 2.121-2.121 1.414 1.414zM16.242 6.344l2.122-2.122 1.414 1.414-2.122 2.122zM6.344 7.759 4.223 5.637l1.415-1.414 2.12 2.122zm13.434 10.605-1.414 1.414-2.122-2.122 1.414-1.414z"></path></svg>`;
+
+
+export function updateDarkMode(label: Element) {
+  document.documentElement.classList.toggle("dark");
+  const isDark = document.documentElement.classList.contains("dark")
+    ? "dark"
+    : "light";
+  localStorage.setItem("Mode", isDark);
+  const icono = isDark === "dark" ? darkIcon : lightIcon 
+  label.innerHTML = icono
+}
